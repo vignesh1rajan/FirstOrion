5d9fcbcb5b7ec2bc75481ad27f057f1883eed19a
vrajan <vigrajan@euronetworldwide.com>
2016-11-14 09:33:21 -0600

    New API tests for validate get

--- src/test/java/vertx/ApiTest.java
+++ src/test/java/vertx/ApiTest.java
@@ -1,8 +1,11 @@
 package vertx;
 
 import com.jayway.restassured.RestAssured;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 import static com.jayway.restassured.RestAssured.given;
 
@@ -13,23 +16,55 @@ import static com.jayway.restassured.RestAssured.given;
 public class ApiTest {
 
     final String baseUri = "http://localhost:8080/";
-    String basePath = "/products";
-
+    String basePath = "/persons";
+    ArrayList<String> nameList = new ArrayList<>(3);
     @BeforeClass
     public void setUpTest(){
         RestAssured.baseURI = baseUri;
         RestAssured.basePath = basePath;
-
+        nameList.add("Dr. Strange");
+        nameList.add("Monty Python");
+        nameList.add("Iron man");
     }
 
     @Test
     public void getAll(){
-        given().log().all()
-               .when().get("/")
-               .then().log().body();
 
+        ArrayList<String > jsonArray =
+            given().
+                    log().all().
+            when().
+                    get("/").
+            then().
+                    log().all().
+                    statusCode(200).
+            extract().
+                    path("name");
 
+        jsonArray.forEach(s -> {
+            try {
+                Assert.assertTrue(nameList.contains(s));
+            }catch (AssertionError ex){
+                System.out.println( "Name not found " + s);
+                throw ex;
+            }
+        });
     }
+/*
+    @Test(dependsOnMethods = "getAll" )
+    public void addPerson(){
+        String newPersondId = "newPerson123";
+
+         given().
+                log().all()
+         .when().
+                put("/" +newPersondId ).
+
+         then().
+                log().all().
+                statusCode(200);
+    }
+    */
 
 
 }
